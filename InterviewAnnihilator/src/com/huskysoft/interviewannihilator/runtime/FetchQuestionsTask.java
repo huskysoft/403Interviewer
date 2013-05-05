4bef0911395889c784c624a45599ee51936d7253
Cody Andrews <codymandrews@gmail.com>
2013-05-04 19:20:08 -0700

    Hooked up front end and back end

---
+++ InterviewAnnihilator/src/com/huskysoft/interviewannihilator/runtime/FetchQuestionsTask.java
@@ -0,0 +1,72 @@
+package com.huskysoft.interviewannihilator.runtime;
+
+import java.util.List;
+
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import com.huskysoft.interviewannihilator.model.NetworkException;
+import com.huskysoft.interviewannihilator.model.Question;
+import com.huskysoft.interviewannihilator.service.QuestionService;
+import com.huskysoft.interviewannihilator.ui.MainActivity;
+import com.huskysoft.interviewannihilator.util.PaginatedQuestions;
+
+/**
+ * 
+ * This AsyncTask is used to populate the MainActivity list of questions.
+ * To use it, call new FetchQuestionsTask(this).execute();
+ * 
+ * @author Cody Andrews
+ *
+ */
+public class FetchQuestionsTask extends AsyncTask<Void, Void, Void>{
+
+    private QuestionService questionService;
+    private MainActivity context;
+    private List<Question> questionList;
+    
+    /**
+     * 
+     * @param context reference to MainActivity
+     */
+    public FetchQuestionsTask(Activity context){
+    	this.context = (MainActivity)context;
+    }
+
+    
+    /**
+     * This is the main function of the AsyncTask thread. This will populate
+     * questionList with Questions so that they may be displayed afterwards.
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+    	questionService = QuestionService.getInstance();
+    	
+		try {
+			PaginatedQuestions currentQuestions =
+					questionService.getQuestions(null, null, 20, 0);
+			questionList = currentQuestions.getQuestions();
+		} catch (NetworkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+    }
+    
+    /**
+     * This event fires when doInBackground() is complete, and it will populate
+     * the MainActivity question area.
+     */
+    @Override
+    protected void onPostExecute(Void result){
+		for (int i = 0; i < questionList.size(); i++) {
+			context.displayQuestion(questionList.get(i));
+		}
+    }
+ }
---
+++ InterviewAnnihilator/src/com/huskysoft/interviewannihilator/runtime/FetchSolutionsTask.java
@@ -0,0 +1,77 @@
+package com.huskysoft.interviewannihilator.runtime;
+
+import java.util.List;
+
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import com.huskysoft.interviewannihilator.model.NetworkException;
+import com.huskysoft.interviewannihilator.model.Question;
+import com.huskysoft.interviewannihilator.model.Solution;
+import com.huskysoft.interviewannihilator.service.QuestionService;
+import com.huskysoft.interviewannihilator.ui.MainActivity;
+import com.huskysoft.interviewannihilator.ui.SolutionActivity;
+import com.huskysoft.interviewannihilator.util.PaginatedQuestions;
+import com.huskysoft.interviewannihilator.util.PaginatedSolutions;
+
+/**
+ * 
+ * This AsyncTask is used to populate SolutionActivity
+ * 
+ * @author Cody Andrews
+ *
+ */
+public class FetchSolutionsTask extends AsyncTask<Void, Void, Void>{
+
+    private QuestionService questionService;
+    private SolutionActivity context;
+    private List<Solution> solutionList;
+    private Question question;
+    
+    /**
+     * 
+     * @param context reference to SolutionActivity
+     * @param question question to find solutions for
+     */
+    public FetchSolutionsTask(Activity context, Question question){
+    	this.context = (SolutionActivity)context;
+    	this.question = question;
+    }
+
+    /**
+     * This is the main function of the AsyncTask thread. This will populate
+     * solutionList with Solutions so that they may be displayed afterwards.
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+    	questionService = QuestionService.getInstance();
+    	
+		try {
+			PaginatedSolutions paginatedSolutions =
+					questionService.getSolutions(question.getId(), 10, 0);
+			
+			solutionList = paginatedSolutions.getSolutions();
+		} catch (NetworkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+    }
+    
+    /**
+     * This event fires when doInBackground() is complete, and it will populate
+     * the MainActivity question area.
+     */
+    @Override
+    protected void onPostExecute(Void result){
+		for (int i = 0; i < solutionList.size(); i++) {
+			context.displaySolution(solutionList.get(i));
+		}
+    }
+ }
