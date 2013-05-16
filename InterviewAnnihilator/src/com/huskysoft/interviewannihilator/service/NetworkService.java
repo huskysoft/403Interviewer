b15b572c4a3fb8bc202bb987e98224021f66bb77
Kevin Loh <kevinloh@uw.edu>
2013-05-16 02:51:51 -0700

    Implemented postQuestion and postSolution in NetworkService.java. Added URLs in NetworkConstants.java and changed the class to an interface.

--- InterviewAnnihilator/src/com/huskysoft/interviewannihilator/service/NetworkService.java
+++ InterviewAnnihilator/src/com/huskysoft/interviewannihilator/service/NetworkService.java
@@ -16,6 +16,8 @@ import java.util.Collection;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.accounts.NetworkErrorException;
@@ -57,14 +59,13 @@ public class NetworkService {
 	 */
 	public String getQuestions(Difficulty difficulty,
 			Collection<Category> categories, int limit, int offset,
-			boolean random)
-			throws NetworkException {
+			boolean random) throws NetworkException {
 		StringBuilder urlToSend = new StringBuilder(GET_QUESTIONS_URL + "?");
 		urlToSend.append(appendParameter(PARAM_LIMIT, String.valueOf(limit)));
 		urlToSend.append(appendParameter(PARAM_OFFSET, String.valueOf(offset)));
 		if (difficulty != null) {
-			urlToSend.append(appendParameter
-					(PARAM_DIFFICULTY, difficulty.name()));
+			urlToSend.append(appendParameter(PARAM_DIFFICULTY,
+					difficulty.name()));
 		}
 		if (categories != null && categories.size() != 0) {
 			Object[] categoryObj = categories.toArray();
@@ -82,10 +83,10 @@ public class NetworkService {
 		if (random) {
 			urlToSend.append(appendParameter(PARAM_RANDOM, ""));
 		}
-		
+
 		// delete the trailing ampersand from the url
 		urlToSend.deleteCharAt(urlToSend.lastIndexOf(AMPERSAND));
-		
+
 		return dispatchGetRequest(urlToSend.toString());
 	}
 
@@ -109,11 +110,35 @@ public class NetworkService {
 				String.valueOf(questionId)));
 		urlToSend.append(appendParameter(PARAM_LIMIT, String.valueOf(limit)));
 		urlToSend.append(appendParameter(PARAM_OFFSET, String.valueOf(offset)));
-		
+
 		// delete the trailing ampersand from the url
 		urlToSend.deleteCharAt(urlToSend.lastIndexOf(AMPERSAND));
-		
+
 		return dispatchGetRequest(urlToSend.toString());
+	}
+
+	/**
+	 * Posts a question to the server. Returns true if the post succeeds.
+	 * 
+	 * @param question
+	 *            a JSON string representing the question
+	 * @return true if the post succeeds, false otherwise
+	 * @throws NetworkException
+	 */
+	public boolean postQuestion(String question) throws NetworkException {
+		return dispatchPostRequest(POST_QUESTION_URL, question);
+	}
+
+	/**
+	 * Posts a solution to the server. Returns true if the post succeeds.
+	 * 
+	 * @param solution
+	 *            a JSON string representing the solution
+	 * @return true if the post succeeds, false otherwise
+	 * @throws NetworkException
+	 */
+	public boolean postSolution(String solution) throws NetworkException {
+		return dispatchPostRequest(POST_SOLUTION_URL, solution);
 	}
 
 	/**
@@ -127,7 +152,7 @@ public class NetworkService {
 	 *            should be set to false if this is the last param that is to be
 	 *            appended
 	 * @return a new String with the parameter appended. Returns the empty
-	 *            String if either of the Strings passed in were null
+	 *         String if either of the Strings passed in were null
 	 */
 	private String appendParameter(String paramName, String paramVal) {
 		if (paramName == null || paramVal == null) {
@@ -167,7 +192,37 @@ public class NetworkService {
 			}
 			return serverString.toString();
 		} catch (Exception e) {
-			throw new NetworkException("Request to " + url + " failed", e);
+			throw new NetworkException("GET request to " + url + " failed", e);
+		}
+	}
+
+	/**
+	 * Dispatches a post request to the remote server with the given url. The
+	 * content of the post request is the given json string.
+	 * 
+	 * @param url
+	 *            the url of the server
+	 * @param content
+	 *            a JSON string as the content of the post request
+	 * @return true if the post request is successful, false otherwise
+	 * @throws NetworkException
+	 */
+	private boolean dispatchPostRequest(String url, String content)
+			throws NetworkException {
+		try {
+			// Create and execute the HTTP POST request
+			HttpPost request = new HttpPost(url);
+			StringEntity requestContent = new StringEntity(content);
+			requestContent.setContentType("application/json");
+			request.setEntity(requestContent);
+			HttpResponse response = httpClient.execute(request);
+
+			// Check the status of the response
+			int statusCode = response.getStatusLine().getStatusCode();
+			return statusCode == 200;
+
+		} catch (Exception e) {
+			throw new NetworkException("POST request to " + url + " failed", e);
 		}
 	}
 
--- InterviewAnnihilator/src/com/huskysoft/interviewannihilator/util/NetworkConstants.java
+++ InterviewAnnihilator/src/com/huskysoft/interviewannihilator/util/NetworkConstants.java
@@ -7,10 +7,10 @@
 
 package com.huskysoft.interviewannihilator.util;
 
-public class NetworkConstants {
+public interface NetworkConstants {
 	/** The URI of our server for both solution and question. */
 	public static final String BASE_URI = 
-	"http://students.washington.edu/bkng/cse403/403Interviewer-php/";
+			"http://students.washington.edu/bkng/cse403/403Interviewer-php/";
 
 	/** The 'question' directory */
 	public static final String QUESTION = "question/";
@@ -19,24 +19,33 @@ public class NetworkConstants {
 	public static final String SOLUTION = "solution/";
 
 	/** The script for getQuestions */
-	public static final String GET_QUESTIONS_URL =
-	BASE_URI + QUESTION + "getQuestions.php";
+	public static final String GET_QUESTIONS_URL = 
+			BASE_URI + QUESTION	+ "getQuestions.php";
 
 	/** The script for getSolutions */
 	public static final String GET_SOLUTIONS_URL = 
-	BASE_URI + SOLUTION + "getSolutions.php";
+			BASE_URI + SOLUTION	+ "getSolutions.php";
 
-	/** If a request for multiple categories happens, this token
-	    separates out the categories */
-	public static final String CATEGORY_DELIMITER = 
-	"-";
+	/** The script for postQuestion */
+	public static final String POST_QUESTION_URL = 
+			BASE_URI + QUESTION	+ "postQuestion.php";
 	
+	/** The script for postSolution */
+	public static final String POST_SOLUTION_URL = 
+			BASE_URI + SOLUTION	+ "postSolution.php";
+
+	/**
+	 * If a request for multiple categories happens, this token separates out
+	 * the categories
+	 */
+	public static final String CATEGORY_DELIMITER = "-";
+
 	/** Ampersand for get URL requests */
 	public static final String AMPERSAND = "&";
-	
+
 	/** HTTP parameters */
 	public static final String PARAM_LIMIT = "limit";
-	public static final String PARAM_OFFSET = "offset";	
+	public static final String PARAM_OFFSET = "offset";
 	public static final String PARAM_DIFFICULTY = "difficulty";
 	public static final String PARAM_CATEGORY = "category";
 	public static final String PARAM_QUESTIONID = "questionId";
