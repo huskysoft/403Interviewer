a13d44d132d38972ed2b769fd836a5873d9441a3
Kevin Loh <kevinloh@uw.edu>
2013-05-19 18:55:24 -0700

    Added unit tests in QuestionService.java for testing param. Added jmock library.

--- InterviewAnnihilatorTest/src/com/huskysoft/interviewannihilator/service/QuestionServiceTest.java
+++ InterviewAnnihilatorTest/src/com/huskysoft/interviewannihilator/service/QuestionServiceTest.java
@@ -17,6 +17,7 @@ import org.json.JSONException;
 import android.os.Environment;
 
 import com.huskysoft.interviewannihilator.model.NetworkException;
+import com.huskysoft.interviewannihilator.model.Question;
 import com.huskysoft.interviewannihilator.model.UserInfo;
 import com.huskysoft.interviewannihilator.util.PaginatedQuestions;
 import com.huskysoft.interviewannihilator.util.PaginatedSolutions;
@@ -29,7 +30,6 @@ import junit.framework.TestCase;
 public class QuestionServiceTest extends TestCase {
 
 	private QuestionService questionService;
-	private ObjectMapper mapper;
 
 	/**
 	 * Construct new test instance
@@ -40,53 +40,6 @@ public class QuestionServiceTest extends TestCase {
 	public QuestionServiceTest(String name) {
 		super(name);
 		questionService = QuestionService.getInstance();
-		mapper = new ObjectMapper();
-	}
-
-	public void testGetAllQuestions() throws NetworkException, JSONException,
-			IOException {
-		PaginatedQuestions questions = questionService.getQuestions(null, null,
-				10, 0, false);
-		assertNotNull(questions);
-		assertEquals(Math.min(10, questions.getTotalNumberOfResults()),
-				questions.getQuestions().size());
-		System.out.println(questions);
-	}
-
-	public void testGetSolutions() throws NetworkException, JSONException,
-			IOException {
-		PaginatedSolutions solutions = questionService.getSolutions(10, 10, 0);
-		assertNotNull(solutions);
-		assertEquals(Math.min(10, solutions.getTotalNumberOfResults()),
-				solutions.getSolutions().size());
-		System.out.println(solutions);
-	}
-
-	public void testReadWriteUserInfo() throws JsonGenerationException,
-			JsonMappingException, IOException, NetworkException {
-		// write UserInfo to a file
-		UserInfo userInfo = TestHelpers.createDummyUserInfo();
-		String json = mapper.writeValueAsString(userInfo);
-		File path = Environment.getExternalStorageDirectory();
-		File file = new File(path, Utility.USER_INFO_FILENAME);
-		Utility.writeStringToFile(file, json);
-		try {
-			// load UserInfo in QuestionService
-			questionService.initializeUserInfo(path, userInfo.getUserEmail());
-
-			// modify userInfo and write changes
-			questionService.clearAllFavorites();
-			questionService.writeUserInfo();
-
-			// read from file and verify changes
-			String str = Utility.readStringFromFile(file);
-			UserInfo clone = mapper.readValue(str, UserInfo.class);
-			assertFalse(userInfo.equals(clone));
-			userInfo.getFavoriteQuestions().clear();
-			assertEquals(userInfo, clone);
-		} finally {
-			file.delete();
-		}
 	}
 
 	/**
@@ -103,6 +56,86 @@ public class QuestionServiceTest extends TestCase {
 			QuestionService service = QuestionService.getInstance();
 			service.postQuestion(null);
 			Assert.fail("Posting null question should not be allowed.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	/**
+	 * Tests that postQuestion throws IllegalArgumentException when given a
+	 * question object with an empty text field. (White box test)
+	 * 
+	 * @throws IOException
+	 * @throws JSONException
+	 * @throws NetworkException
+	 */
+	public void testpostQuestionEmptyText() throws NetworkException,
+			JSONException, IOException {
+		try {
+			QuestionService service = QuestionService.getInstance();
+			Question question = TestHelpers.createDummyQuestion(0);
+			question.setText("");
+			service.postQuestion(question);
+			Assert.fail("Posting question with empty text is disallowed.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	
+	/**
+	 * Tests that postQuestion throws IllegalArgumentException when given a
+	 * question object with an empty title field. (White box test)
+	 * 
+	 * @throws IOException
+	 * @throws JSONException
+	 * @throws NetworkException
+	 */
+	public void testpostQuestionEmptyTitle() throws NetworkException,
+			JSONException, IOException {
+		try {
+			QuestionService service = QuestionService.getInstance();
+			Question question = TestHelpers.createDummyQuestion(0);
+			question.setTitle("");
+			service.postQuestion(question);
+			Assert.fail("Posting question with empty title is disallowed.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	
+	/**
+	 * Tests that postQuestion throws IllegalArgumentException when given a
+	 * question object with a null category field. (White box test)
+	 * 
+	 * @throws IOException
+	 * @throws JSONException
+	 * @throws NetworkException
+	 */
+	public void testpostQuestionNullCategory() throws NetworkException,
+			JSONException, IOException {
+		try {
+			QuestionService service = QuestionService.getInstance();
+			Question question = TestHelpers.createDummyQuestion(0);
+			question.setCategory(null);
+			service.postQuestion(question);
+			Assert.fail("Posting question with null category is disallowed.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	
+	/**
+	 * Tests that postQuestion throws IllegalArgumentException when given a
+	 * question object with a null difficulty field. (White box test)
+	 * 
+	 * @throws IOException
+	 * @throws JSONException
+	 * @throws NetworkException
+	 */
+	public void testpostQuestionNullDifficulty() throws NetworkException,
+			JSONException, IOException {
+		try {
+			QuestionService service = QuestionService.getInstance();
+			Question question = TestHelpers.createDummyQuestion(0);
+			question.setDifficulty(null);
+			service.postQuestion(question);
+			Assert.fail("Posting question with null difficulty is disallowed.");
 		} catch (IllegalArgumentException e) {
 		}
 	}
