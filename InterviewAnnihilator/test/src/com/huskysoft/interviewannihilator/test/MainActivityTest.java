2397dc898e595448bd25b0b007b213994ce23dba
Phillip Leland <pleland17@gmail.com>
2013-06-01 13:48:46 -0700

    Style changes

--- InterviewAnnihilator/test/src/com/huskysoft/interviewannihilator/test/MainActivityTest.java
+++ InterviewAnnihilator/test/src/com/huskysoft/interviewannihilator/test/MainActivityTest.java
@@ -1,3 +1,10 @@
+/**
+ * Tests for the MainActivity. Checks difficulty, categories,
+ * adding of questions.
+ * 
+ * @author Phillip Leland
+ */
+
 package com.huskysoft.interviewannihilator.test;
 
 import java.util.Date;
@@ -15,24 +22,23 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import com.huskysoft.interviewannihilator.R;
 import com.huskysoft.interviewannihilator.model.Category;
 import com.huskysoft.interviewannihilator.model.Difficulty;
 import com.huskysoft.interviewannihilator.model.Question;
 import com.huskysoft.interviewannihilator.ui.MainActivity;
 
 public class MainActivityTest extends
-		ActivityInstrumentationTestCase2<MainActivity> {
+ActivityInstrumentationTestCase2<MainActivity> {
 
 	private MainActivity mActivity;
 	private Spinner mSpinner;
 	private TableLayout mTable;
-	
+
 	public MainActivityTest() {
 		super(MainActivity.class);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -42,13 +48,13 @@ public class MainActivityTest extends
 		mActivity = getActivity();
 
 		mSpinner =
-			(Spinner) mActivity.findViewById(
-			com.huskysoft.interviewannihilator.R.id.diff_spinner);
-		
+				(Spinner) mActivity.findViewById(
+				com.huskysoft.interviewannihilator.R.id.diff_spinner);
+
 		mTable = (TableLayout) mActivity.findViewById(
-			com.huskysoft.interviewannihilator.R.id.slide_table);
+				com.huskysoft.interviewannihilator.R.id.slide_table);
 	}
-	
+
 	/**
 	 * Tests the preconditions for the MainActivity.
 	 * 
@@ -60,7 +66,7 @@ public class MainActivityTest extends
 		assertEquals(0, ll.getChildCount());
 		assertEquals(4, mSpinner.getCount());
 	}
-	
+
 	/**
 	 * Tests the getCurrentDifficultySetting method.
 	 * Checks to make sure the method returns null which
@@ -72,7 +78,7 @@ public class MainActivityTest extends
 		Difficulty diff = mActivity.getCurrentDifficultySetting();
 		assertNull(diff);
 	}
-	
+
 	/**
 	 * Tests the getCurrentDifficultySetting method.
 	 * 
@@ -83,32 +89,32 @@ public class MainActivityTest extends
 	 */
 	public void testDifficultySelection(){
 		mActivity.runOnUiThread(
-			new Runnable() {
-				public void run() {
-					mSpinner.requestFocus();
-					mSpinner.setSelection(3);
-				} // end of run() method definition
-			} // end of anonymous Runnable object instantiation
-		);
-		
+				new Runnable() {
+					public void run() {
+						mSpinner.requestFocus();
+						mSpinner.setSelection(3);
+					} // end of run() method definition
+				} // end of anonymous Runnable object instantiation
+				);
+
 		this.getInstrumentation().waitForIdleSync(); // added
-		
+
 		this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
-	    for (int i = 1; i <= 3; i++) {
-	      this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
-	    } // end of for loop
+		for (int i = 1; i <= 3; i++) {
+			this.sendKeys(KeyEvent.KEYCODE_DPAD_DOWN);
+		} // end of for loop
 
-	    this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
-	    
-	    int mPos = mSpinner.getSelectedItemPosition();
-	    String mSelection = (String) mSpinner.getItemAtPosition(mPos);
+		this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
 
-	    Difficulty diff = mActivity.getCurrentDifficultySetting();
-	    String returnedDiff = diff.toString();
-	    
-	    assertEquals(mSelection.toUpperCase(Locale.getDefault()), returnedDiff);
+		int mPos = mSpinner.getSelectedItemPosition();
+		String mSelection = (String) mSpinner.getItemAtPosition(mPos);
+
+		Difficulty diff = mActivity.getCurrentDifficultySetting();
+		String returnedDiff = diff.toString();
+
+		assertEquals(mSelection.toUpperCase(Locale.getDefault()), returnedDiff);
 	}
-	
+
 	/**
 	 * Tests the setDifficulty method of MainActivity. Makes sure
 	 * it selects the correct difficulty enum.
@@ -118,14 +124,14 @@ public class MainActivityTest extends
 	@UiThreadTest
 	public void testSetDifficultyEnum(){
 		mActivity.setDifficultyToSelectedValue("HARD");
-		
+
 		Difficulty diff = mActivity.getCurrentDifficultySetting();
-	    String returnedDiff = diff.toString();
-	    
-	    assertEquals("HARD", returnedDiff.toUpperCase());
-	    
+		String returnedDiff = diff.toString();
+
+		assertEquals("HARD", returnedDiff.toUpperCase());
+
 	}
-	
+
 	/**
 	 * Test that tests that setDifficulty displays the correct
 	 * value in the spinner.
@@ -135,13 +141,13 @@ public class MainActivityTest extends
 	@UiThreadTest
 	public void testSetDifficultyDisplayed(){
 		mActivity.setDifficultyToSelectedValue("HARD");
-		
+
 		int mPos = mSpinner.getSelectedItemPosition();
-	    String mSelection = (String) mSpinner.getItemAtPosition(mPos);
-	    
-	    assertEquals("HARD", mSelection.toUpperCase());
+		String mSelection = (String) mSpinner.getItemAtPosition(mPos);
+
+		assertEquals("HARD", mSelection.toUpperCase());
 	}
-	
+
 	/**
 	 * Test that checks the setting of one category. One category passed
 	 * in a list to the setCategorySpinner method. Checks to see if 
@@ -153,17 +159,18 @@ public class MainActivityTest extends
 	public void testSetCategorySingleNumSpinners(){
 		List<Category> cats = new LinkedList<Category>();
 		cats.add(Category.COMPSCI);
-		
+
 		mActivity.setCategorySpinners(cats);
-		 
+
 		int tableChildren = mTable.getChildCount();
-		 
+
 		assertEquals(3, tableChildren);
 	}
-	
+
 	/**
-	 * Test that checks the setting of multiple categorys. List of category passed
-	 * in a list to the setCategorySpinner method. Checks to see if 
+	 * Test that checks the setting of multiple categorys. 
+	 * List of category passed in a list to the
+	 * setCategorySpinner method. Checks to see if 
 	 * that the correct number of spinners is displayed.
 	 * 
 	 * @label white box
@@ -171,17 +178,17 @@ public class MainActivityTest extends
 	@UiThreadTest
 	public void testSetCategoryMultipleNumSpinners(){
 		List<Category> cats = new LinkedList<Category>();
-		
+
 		cats.add(Category.COMPSCI);
 		cats.add(Category.BUSINESS);
-		
+
 		mActivity.setCategorySpinners(cats);
-		 
+
 		int tableChildren = mTable.getChildCount();
-		 
+
 		assertEquals(4, tableChildren);
 	}
-	
+
 	/**
 	 * Sets one category spinner. Checks to see that
 	 * string value is pre-selected.
@@ -191,21 +198,21 @@ public class MainActivityTest extends
 	@UiThreadTest
 	public void testSetCategorySingleSpinnerValue(){
 		List<Category> cats = new LinkedList<Category>();
-		
+
 		cats.add(Category.COMPSCI);
 		cats.add(Category.BUSINESS);
-		
+
 		mActivity.setCategorySpinners(cats);
-		
+
 		TableRow r = (TableRow) mTable.getChildAt(1);
 		Spinner s = (Spinner) r.getChildAt(1);
 		int mPos = s.getSelectedItemPosition();
-	    String mSelection = (String) s.getItemAtPosition(mPos);
-	    
-	    assertEquals("COMPSCI", mSelection.toUpperCase());
+		String mSelection = (String) s.getItemAtPosition(mPos);
+
+		assertEquals("COMPSCI", mSelection.toUpperCase());
 	}
-	
-	
+
+
 	/**
 	 * Sends a null list to AppendQuestionsToView and
 	 * checks to make sure the correct message is displayed.
@@ -217,20 +224,20 @@ public class MainActivityTest extends
 		ViewGroup questionView =
 				(ViewGroup) mActivity.findViewById(
 				com.huskysoft.interviewannihilator.R.id.question_layout);
-		
+
 		questionView.removeAllViews();
-		
+
 		mActivity.appendQuestionsToView(null);
-		
+
 		assertEquals(1, questionView.getChildCount());
-		
+
 		TextView t = (TextView) questionView.getChildAt(0);
 		String message = (String) t.getText();
 		String expected = "There doesn't seem to be any questions.";
-		
+
 		assertEquals(expected, message);
 	}
-	
+
 	/**
 	 * Populates the view with one question by using appendQuestionsToView.
 	 * Makes sure that question is displayed.
@@ -242,16 +249,16 @@ public class MainActivityTest extends
 		ViewGroup questionView =
 				(ViewGroup) mActivity.findViewById(
 				com.huskysoft.interviewannihilator.R.id.question_layout);
-		
+
 		questionView.removeAllViews();
-		
+
 		Question test = new Question(
 				"test", "test", Category.COMPSCI, Difficulty.EASY);
 		test.setDateCreated(new Date());
 		List<Question> q = new LinkedList<Question>();
 		q.add(test);
 		mActivity.appendQuestionsToView(q);
-		
+
 		assertEquals(1, questionView.getChildCount());		
 	}
 }
