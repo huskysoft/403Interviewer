69453eceecf0ca2045227f999abf71c2da797edd
Cody Andrews <codymandrews@gmail.com>
2013-06-02 13:43:00 -0700

    moved AbstractPostingActivityTest to ui package

--- InterviewAnnihilator/test/src/com/huskysoft/interviewannihilator/ui/AbstractPostingActivityTest.java
+++ InterviewAnnihilator/test/src/com/huskysoft/interviewannihilator/ui/AbstractPostingActivityTest.java
@@ -6,7 +6,7 @@
  * @author Phillip Leland
  */
 
-package com.huskysoft.interviewannihilator.test;
+package com.huskysoft.interviewannihilator.ui;
 
 import java.util.LinkedList;
 import java.util.List;
