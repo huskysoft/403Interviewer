5433a633064d7263425999c999d9449e6fc7a158
Cody Andrews <codymandrews@gmail.com>
2013-06-05 22:17:28 -0700

    refactored ui code in QuestionActivity.revealSolutions

--- InterviewAnnihilator/src/com/huskysoft/interviewannihilator/ui/MainActivity.java
+++ InterviewAnnihilator/src/com/huskysoft/interviewannihilator/ui/MainActivity.java
@@ -32,7 +32,6 @@ import android.view.Window;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
